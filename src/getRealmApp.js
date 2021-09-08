2a321b8a88dedb443f67282d3f207af75b3d340b
NIASKA <mkman751@yahoo.com>
2021-09-08 15:33:57 -0500

    inital files

---
+++ src/getRealmApp.js
@@ -0,0 +1,19 @@
+import realm from 'realm';
+
+let app;
+
+export function getRealmApp() {
+  if (app === undefined) {
+    const appId = "premiumbandai-jxgfe";
+    const appConfig = {
+      id: appId,
+      timeout: 10000,
+      app: {
+        name: "default",
+        version: "0",
+      },
+    };
+    app = new Realm.App(appConfig);
+  }
+  return app;
+}
