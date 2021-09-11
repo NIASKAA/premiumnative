bffdba0380aa887ac257b99bafc4018c17fa4f8b
NIASKA <mkman751@yahoo.com>
2021-09-10 19:04:58 -0500

    i think i did it

--- client/src/apollo.js
+++ client/src/apollo.js
@@ -3,7 +3,7 @@ import { setContext } from '@apollo/client/link/context';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:4000/graphql',
+    uri: 'https://premiumnative.herokuapp.com/graphql',
 });
 
 const authLink = setContext(async (_, { headers }) => {
