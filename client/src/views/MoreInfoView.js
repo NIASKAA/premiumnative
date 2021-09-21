72bab230f71bf77e12c0d3b2b4a89cf987961a00
NIASKA <mkman751@yahoo.com>
2021-09-21 18:23:37 -0500

    testing

--- client/src/views/MoreInfoView.js
+++ client/src/views/MoreInfoView.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {StyleSheet} from 'react-native'
 import FastImage from 'react-native-fast-image'
+import {useMutation} from '@apollo/client'
 import {Text, Body, Card, CardItem, Left, Button} from 'native-base'
+import {DELETE_CONVERGE_WISHLIST, 
+    DELETE_HIGHGRADE_WISHLIST, 
+    DELETE_REALGRADE_WISHLIST, 
+    DELETE_MASTERGRADE_WISHLIST, 
+    DELETE_PERFECT_WISHLIST, 
+    DELETE_SDGRADE_WISHLIST,
+    DELETE_OTHER_WISHLIST
+} from '../utils/mutations'
+
 
 const MoreInfoView = ({navigation, route}) => {
     const {gunplaName, image, releaseDate, price, series, _id} = route.params
+    const [deleteConvergeWishlist] = useMutation(DELETE_CONVERGE_WISHLIST)
+
+    const deleteItem = (id) => {
+        try {
+            deleteConvergeWishlist({
+                variables: {
+                    convergeID: id
+                }
+            })
+        } catch (error) {
+            console.log(error)
+        }
+        window.location.reload();   
+    }
 
     console.log(route.params)
     return (
@@ -23,7 +47,7 @@ const MoreInfoView = ({navigation, route}) => {
                         </Body>
                     </Left>
                 </CardItem>
-                <Button style={styles.delBtn}><Text>Delete</Text></Button>
+                <Button style={styles.delBtn} onPress={() => deleteItem(_id)}><Text>Delete</Text></Button>
             </Card>
         </>
     )
