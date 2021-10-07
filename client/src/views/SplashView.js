6a89f2dbde3445974caa9fdee7b3d9b810821e11
NIASKA <mkman751@yahoo.com>
2021-10-07 18:52:41 -0500

    fixing splash page

--- client/src/views/SplashView.js
+++ client/src/views/SplashView.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react'
 import {Content, Spinner} from 'native-base'
 import {useNavigation} from '@react-navigation/native'
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import decode from 'jwt-decode'
 
 const SplashView = () => {
     const navigation = useNavigation()
@@ -17,10 +18,29 @@ const SplashView = () => {
     
         checkUser();
     }, []);
+
+    const isTokenExpired = (token) => {
+      try {
+          const decoded = decode(token);
+          if(decoded.exp < Date.now() / 1000) {
+              return true
+          } else return false;
+      } catch (error) {
+          return false
+      }
+    }
+    
+    const setCredentials = async (token) => {
+      try {
+        await AsyncStorage.setItem('token', JSON.stringify(token))
+      } catch (error) {
+        console.log(error)
+      }
+    }
     
     const isAuthenticated = async () => {
-    const token = await AsyncStorage.getItem('token');
-    return !!token;
+      const token = await AsyncStorage.getItem('token');
+      return !!token;
     }
 
     return (
